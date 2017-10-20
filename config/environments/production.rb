5c79e6e1428d669b52b48ecc6a25a15a0e7a4b5c
huuphuc93 <huuphuc.ns@gmail.com>
2017-10-20 20:47:38 +0700

    chap11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'https://mysterious-eyrie-69332.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     :address          => 'smtp.gmail.com',
     :port                 => '587',
