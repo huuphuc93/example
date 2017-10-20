343771839b83f9ea554a7b167f1316a98f14ef43
huuphuc93 <huuphuc.ns@gmail.com>
2017-10-20 20:38:30 +0700

    chap11

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,7 +4,8 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 140 }
-  validates :picture_size
+  validate :picture_size
+
   private
     def picture_size
       if picture.size > 5.megabytes
